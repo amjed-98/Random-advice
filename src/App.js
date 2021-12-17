aa8157e1e8fdfd062d36e3b2171d118770239133
Amjad Yahia <amjedyehia1998@gmail.com>
2021-12-17 18:06:11 +0200

    fixed fetching error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
 
   fetchAdvice = () => {
     const sendReq = async () => {
-      const id = Math.floor(Math.random() * 100);
+      const id = Math.floor(Math.random() * 100) - 1;
       const res = await axios.get(`https://api.adviceslip.com/advice/${id}`);
 
       // if error
